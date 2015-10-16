ed92e8ecd6eb574175de94960e59d72b2bb661f2
qingfengwuyou <h3658686@126.com>
2015-10-17 01:47:58 +0800

    no message

--- server/app.js
+++ server/app.js
@@ -21,15 +21,16 @@ app.set('view engine', 'jade');
 app.locals.pretty = true;
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', function(req, res) {
   ModelCar.fetch(function(err, cars) {
-
     if (err) {
-      return next(err);
+      console.log(err);
+      return res.sendStatus(500);
     }
-
     res.render('index', {
       title: '汽车商城 首页',
       cars: cars
@@ -40,15 +41,10 @@ app.get('/', function(req, res) {
 
 app.get('/car/:id', function(req, res) {
   var id = req.params.id;
-
-  console.log(app);
   ModelCar.findById(id, function(err, car) {
     if (err) {
-      // console.log(err);
-      //next();
-      
-      res.redirect('/');
-      next(err);
+      console.log(err);
+      return res.redirect('/');
     }
     res.render('car_detail', {
       title: '汽车商城 详情页',
@@ -60,7 +56,8 @@ app.get('/car/:id', function(req, res) {
 app.get('/admin/car/list', function(req, res) {
   ModelCar.fetch(function(err, cars) {
     if (err) {
-      return next(err);
+      console.log(err);
+      return res.redirect('/');
     }
     res.render('car_list.jade', {
       title: '汽车商城 列表页',
@@ -80,7 +77,8 @@ app.get('/admin/car/update/:id', function(req, res) {
   var id = req.params.id;
   ModelCar.findById(id, function(err, car) {
     if (err) {
-      return next(err);
+      console.log(err);
+      return res.redirect('/');
     }
     res.render('car_admin', {
       title: '汽车商城 后台录入页',
@@ -89,10 +87,57 @@ app.get('/admin/car/update/:id', function(req, res) {
   });
 });
 
-app.post('/admin/car',function(req,res){
-  console.log(req.body);
+app.post('/admin/car', function(req, res) {
+  var carObj = req.body.car;
+  if (!carObj) {
+    console.log(err);
+    return res.sendStatus(400, '找不到合法数据.');
+  }
+  var id = carObj._id;
+  if (!id) {
+    //新增
+    var docCar = new ModelCar(carObj);
+    docCar.save(function(err, _car) {
+      if (err) {
+        console.log(err);
+        return res.redirect('/');
+      }
+      return res.redirect('/car/' + _car._id);
+    });
+  } else {
+    //修改
+    ModelCar.findByIdAndUpdate(id, carObj, function(err, _car) {
+      if (err) {
+        console.log(err);
+        return res.redirect('/');
+      }
+      return res.redirect('/car/' + id);
+    });
+  }
+});
 
-  res.sendStatus(200);
+// /admin/list?id=xxxxx
+ 
+app.delete('/admin/list', function(req, res) {
+  var id = req.query.id;
+  if (id) {
+    ModelCar.findByIdAndRemove(id, function(err, _car) {
+      if (err) {
+        console.log(err);
+        res.json({
+          ok: 0
+        });
+      } else {
+        res.json({
+          ok: 1
+        });
+      }
+    });
+  } else {
+    res.json({
+      ok: 0
+    });
+  }
 });
 
 
@@ -107,7 +152,3 @@ console.log("汽车商城网站服务已启动,监听端口号:" + port);
 
 var errorhandler = require('errorhandler');
 app.use(errorhandler);
-
-
-
-

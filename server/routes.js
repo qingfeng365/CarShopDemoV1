1f0b19be68fadcd0086d5b9a2bcaa347fc67d085
qingfengwuyou <h3658686@126.com>
2015-10-21 14:55:47 +0800

    04-work mvc重构结束

--- server/routes.js
+++ server/routes.js
@@ -1,112 +1,21 @@
 'use strict';
 
-var ModelCar = require('./models/car');
+var indexController = require('./controllers/index');
+var carController = require('./controllers/car');
 
 module.exports = function(app) {
-  app.get('/', function(req, res, next) {
-    ModelCar.fetch(function(err, cars) {
-      if (err) {
-        return next(err);
-      }
-      res.render('index', {
-        title: '汽车商城 首页',
-        cars: cars
-      });
-    });
-  });
+  app.get('/', indexController.index);
 
-  app.get('/car/:id', function(req, res, next) {
-    var id = req.params.id;
-    ModelCar.findById(id, function(err, car) {
-      if (err) {
-        return next(err);
-      }
-      res.render('car_detail', {
-        title: '汽车商城 详情页',
-        car: car
-      });
-    });
-  });
+  app.get('/car/:id', carController.showDetail);
 
-  app.get('/admin/car/list', function(req, res, next) {
-    ModelCar.fetch(function(err, cars) {
-      if (err) {
-        return next(err);
-      }
-      res.render('car_list.jade', {
-        title: '汽车商城 列表页',
-        cars: cars
-      });
-    });
-  });
+  app.get('/admin/car/list', carController.showList);
 
-  app.get('/admin/car/new', function(req, res) {
-    res.render('car_admin', {
-      title: '汽车商城 后台录入页',
-      car: {}
-    });
-  });
+  app.get('/admin/car/new', carController.new);
 
-  app.get('/admin/car/update/:id', function(req, res, next) {
-    var id = req.params.id;
-    ModelCar.findById(id, function(err, car) {
-      if (err) {
-        return next(err);
-      }
-      res.render('car_admin', {
-        title: '汽车商城 后台录入页',
-        car: car
-      });
-    });
-  });
+  app.get('/admin/car/update/:id', carController.update);
 
-  app.post('/admin/car', function(req, res, next) {
-    var carObj = req.body.car;
-    if (!carObj) {
-      return res.status(400).send('找不到合法数据.');
-    }
-    var id = carObj._id;
-    if (!id) {
-      //新增
-      var docCar = new ModelCar(carObj);
-      docCar.save(function(err, _car) {
-        if (err) {
-          return next(err);
-        }
-        return res.redirect('/car/' + _car._id);
-      });
-    } else {
-      //修改
-      ModelCar.findByIdAndUpdate(id, carObj, function(err, _car) {
-        if (err) {
-          return next(err);
-        }
-        return res.redirect('/car/' + id);
-      });
-    }
-  });
+  app.post('/admin/car', carController.post);
 
   // /admin/list?id=xxxxx
-
-  app.delete('/admin/list', function(req, res, next) {
-    var id = req.query.id;
-    if (id) {
-      ModelCar.findByIdAndRemove(id, function(err, _car) {
-        if (err) {
-          res.status(500).json({
-            ok: 0
-          });
-          return next(err);
-        } else {
-          res.json({
-            ok: 1
-          });
-        }
-      });
-    } else {
-      res.json({
-        ok: 0
-      });
-    }
-  });
+  app.delete('/admin/list', carController.del);
 };

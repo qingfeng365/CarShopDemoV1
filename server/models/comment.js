bbbcbbe113b90bad5355fc10a765d7f1e7790dd0
qingfengwuyou <h3658686@126.com>
2015-10-28 22:36:48 +0800

    no message

--- server/models/comment.js
+++ server/models/comment.js
@@ -16,6 +16,26 @@ var schemaComment = new mongoose.Schema({
     type: String,
     required: true
   },
+  reply: [{
+    from: {
+      type: ObjectId,
+      ref: 'ModelUser'
+    },
+    to: {
+      type: ObjectId,
+      ref: 'ModelUser'
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    meta: {
+      createDate: {
+        type: Date,
+        default: Date.now()
+      }
+    }
+  }],
   meta: {
     createDate: {
       type: Date,
@@ -25,10 +45,14 @@ var schemaComment = new mongoose.Schema({
 });
 
 schemaComment.statics = {
-  fetchByCarId: function(carId,cb) {
+  fetchByCarId: function(carId, cb) {
     return this
-      .find({car:carId})
+      .find({
+        car: carId
+      })
+      .sort('meta.createDate')
       .populate('from', 'name')
+      .populate('reply.from reply.to', 'name')
       .exec(cb);
   }
 };
